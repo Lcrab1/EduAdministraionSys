9320df11902b9374e66f8c762f4418259a31ece5
Huang Ruishen <156391099+Ration000@users.noreply.github.com>
2024-06-19 14:17:16 +0800

    HRS:小改database

--- EduAdministraionSys/Database.cpp
+++ EduAdministraionSys/Database.cpp
@@ -1,35 +1,34 @@
 ﻿#include"pch.h"
 #include"Database.h"
 
-Database::Database()
-{
-
-}
-
-void Database::searchStudent(CString studentID)
+void Database::login(CString& userName, CString& password)
 {
 }
 
-void Database::searchStudentAllCourse(CString studentID)
+void Database::searchStudent(CString& studentID)
 {
 }
 
-void Database::searchStudentCourseName(CString studentID, CString courseID)
+void Database::searchStudentAllCourse(CString& studentID)
 {
 }
 
-void Database::searchTeacherCourse(CString teacherID)
+void Database::searchStudentCourseName(CString& studentID, CString& courseID)
 {
 }
 
-void Database::searchSemesterCourse(CString semester)
+void Database::searchTeacherCourse(CString& teacherID)
 {
 }
 
-void Database::setGradeComposi(CString CourseID)
+void Database::searchSemesterCourse(CString& semester)
 {
 }
 
-void Database::recordGrade(CString studentGrade, CString courseID)
+void Database::setGradeComposi(CString& CourseID)
+{
+}
+
+void Database::recordGrade(CString& studentGrade, CString& courseID)
 {
 }
